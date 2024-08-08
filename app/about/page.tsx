0d1025afbfcacfb1e4cbb3f8efdbbf6fc9c9a21e
William Ramos <ramowilliam@gmail.com>
2024-08-08 19:06:31 -0300

    chore: add prettier tailwind plugin

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useState } from "react";
+
+//SERVER COMPONENTS
+const About = () => {
+    const [] = useState();
+    return <h1>about page</h1>;
+  };
+  
+  export default About;
